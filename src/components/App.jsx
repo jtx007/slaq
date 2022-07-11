65a3b9fe96d18ed7c0a25a07f3beed0d180c2645
jtx007 <jamesjacobthomas7@gmail.com>
2022-07-11 15:46:47 -0400

    initial project structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
+import logo from "../logo.svg";
 import * as React from "react";
-import "./App.css";
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
+        <h3>James Thomas!</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
